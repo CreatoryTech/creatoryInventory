66d663180449e51dc427a6067e555ccaa8bbf2d9
Sarah Khan <khan.sarah630@gmail.com>
2018-04-11 00:01:24 +0600

    database configuration corrected

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 
 return [
 
@@ -14,11 +19,7 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
+
 
 
     /*
